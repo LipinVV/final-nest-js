c394d5fea557c21546d82c8d9be09ff7f6ab0f2b
LipinVV <vit.lipin@gmail.com>
2024-10-05 17:16:14 +0300

    Initial commit #1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Application is working 13 well';
+    return 'Application is working well';
   }
 }
